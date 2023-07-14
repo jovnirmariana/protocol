e734facb948d963df574de263299d04936eacff3
jovnirmariana <117020108+jovnirmariana@users.noreply.github.com>
2023-07-14 15:38:48 +0100

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,6 @@
+require('hardhat-contract-sizer')
+require('@nomiclabs/hardhat-waffle')
+require('solidity-coverage')
+require('hardhat-gas-reporter')
+require('@nomiclabs/hardhat-etherscan')
+require('dotenv').config()
