fb2cbca98a4c341ab635a323d21452db4b8d42b3
jovnirmariana <117020108+jovnirmariana@users.noreply.github.com>
2023-09-16 10:41:57 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -105,3 +105,6 @@ url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
       chainId: 80001,
  accounts: [process.env.PRIVATE_KEY],
       gasPrice: 2 * GWEI,
+ explorer: 'https://mumbai.polygonscan.com/'
+    },
+    ethereum: {
