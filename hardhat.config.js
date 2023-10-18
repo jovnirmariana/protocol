fd77c6dc256cfa0cc5e9aa322561e6c51136c0a1
jovnirmariana <117020108+jovnirmariana@users.noreply.github.com>
2023-10-18 11:41:04 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -52,3 +52,6 @@ networks: {
       accounts: [process.env.PRIVATE_KEY],
 gasPrice: 1 * GWEI,
       explorer: 'https://goerli.basescan.org'
+ },
+    fuji: {
+      url: 'https://ava-testnet.public.blastapi.io/ext/bc/C/rpc',
