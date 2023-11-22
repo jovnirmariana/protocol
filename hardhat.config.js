2e0ec614503fc736a8cd8a74f73184c6f7f620dd
jovnirmariana <117020108+jovnirmariana@users.noreply.github.com>
2023-11-22 12:17:04 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,3 +3,6 @@ require('@nomiclabs/hardhat-waffle')
 require('solidity-coverage')
 require('hardhat-gas-reporter')
 require('@nomiclabs/hardhat-etherscan')
+require('dotenv').config()
+
+const GWEI = 1000000000
